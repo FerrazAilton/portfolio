3c282940c5fff33f4ae3e8444cd176feeab5e1d0
Ailton Ferraz <ferrazailtonn@gmail.com>
2023-03-08 22:55:23 +0000

    firdt commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import { HiArrowCircleUp } from 'react-icons/hi'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/c83cfcc5-b0b7-4aee-9140-a7cf761c7830" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/9254be5d-da5f-42ce-90b8-6cdcda069f01" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#38B6FF] text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email </p>
